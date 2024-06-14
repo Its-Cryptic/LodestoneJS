369b29a4a86e593e639d8b9addcbce4d83aa294f
Cryptic <117136390+Its-Cryptic@users.noreply.github.com>
2024-06-14 01:58:13 -0700

    The beginning

---
+++ src/main/java/dev/cryptic/lodestonejs/LodestoneJSPlugin.java
@@ -0,0 +1,16 @@
+package dev.cryptic.lodestonejs;
+
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.BindingsEvent;
+
+public class LodestoneJSPlugin extends KubeJSPlugin {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void registerBindings(BindingsEvent event) {
+        //event.add("LodestoneJS", );
+    }
+}
---
+++ src/main/java/dev/cryptic/lodestonejs/kubejs/LodestoneJSEvents.java
@@ -0,0 +1,6 @@
+package dev.cryptic.lodestonejs.kubejs;
+
+import dev.latvian.mods.kubejs.event.EventHandler;
+
+public class LodestoneJSEvents {
+}
