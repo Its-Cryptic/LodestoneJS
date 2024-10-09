94d9ef1ae39f826acc047b31c116af05b4824d1a
Cryptic <117136390+Its-Cryptic@users.noreply.github.com>
2024-10-09 15:58:36 -0700

    script
    Signed-off-by: Cryptic <117136390+Its-Cryptic@users.noreply.github.com>

--- run/kubejs/server_scripts/src/main.js
+++ run/kubejs/server_scripts/src/main.js
@@ -1,5 +1,18 @@
 EntityEvents.death(event => {
     let {level, entity} = event
-    LodestoneJSUtils.spawnWorldEvent('mymod:myworldevent', level)
+    LodestoneJSUtils.spawnWorldEvent('mymod:myworldevent', level, data => {
+        data.write("duration", getDuration())
+    })
     console.log('spawned world event')
 })
+
+let nextValue = 300;
+
+function getDuration() {
+    var v1 = 300;
+    var v2 = 600;
+ 
+    var returnValue = nextValue;
+    nextValue = nextValue == v1 ? v2 : v1;
+    return returnValue;
+}
